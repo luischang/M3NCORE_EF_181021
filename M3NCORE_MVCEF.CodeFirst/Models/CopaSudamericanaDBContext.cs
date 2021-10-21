57bcdd0b024f2762c357be0faf147a2d99ea81da
Luis Chang <luis.chang@outlook.com>
2021-10-20 22:10:51 -0500

    MVC Code First

---
+++ M3NCORE_MVCEF.CodeFirst/Models/CopaSudamericanaDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M3NCORE_MVCEF.CodeFirst.Models
+{
+    public class CopaSudamericanaDBContext : DbContext
+    {
+        public CopaSudamericanaDBContext(DbContextOptions<CopaSudamericanaDBContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Player> Player { get; set; }
+
+    }
+}
